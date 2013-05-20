8eea66dbbe03d058513f654ce089c8001348db4b
Duhee Lee <doo2@wafflestudio.com>
2013-05-20 15:34:34 +0900

    fix

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -2,13 +2,18 @@ class ChatController < ApplicationController
   def chat_list
     if params[:id].nil? #no user
       render :text => "No such a user"
-    elsif !params[:content].nil? #new chat is arrived
-      c = Chat.new
-      c.content = params[:content]
-      c.username = params[:id]
-      c.save
     end
 
     @list = Chat.all
+  end
+
+  def store
+    c = Chat.new
+    c.content = params[:content]
+    c.username = params[:id]
+    c.save
+    
+    redirect_to :action => "chat_list", :id => params[:id]
+
   end
 end
